969a83c49b91525fb1c4888d3048f66acf116442
ari0ye <arioye586@gmail.com>
2025-08-06 13:19:23 -0700

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,5 @@
+def greet_user(name):
+  return f"Hello, {name.capitalize()! Welcome to Github Practice}"
+def celsius_to_fahrenheit(celsius)
+fahrenheit = (celsius * 9/2) + 32
+return rouund(fahrenheit, 2)
